7e5f333e80895b927304bcd37e0063af40e2866f
wbeimar <wbeimarmonsalve33@gmail.com>
2025-02-05 11:03:22 -0500

    Loops del curso javascript

---
+++ for-of.js
@@ -0,0 +1,27 @@
+// El bucle for...of en JavaScript se usa para iterar sobre elementos de estructuras iterables como 
+// arreglos, cadenas, Map, Set y otros objetos iterables.
+
+// sintaxis 
+
+// for of arrays, strings [algo]
+
+// for (variable of objeto) {
+//     codigo a ejecutar
+// }
+
+// let canasta = ["manzana", "pera", "naranja", "uva"];
+
+// for (fruta of canasta) {
+//     console.log(fruta);
+// }
+
+let canasta = ["manzana", "pera", "naranja", "uva", "kiwi"];
+
+//Inicialización del contador: se inicializa en 1, se enumera las frutas
+let i = 1;
+
+//Este bucle 'for...of' recorre cada elemento del array en cada iteración 
+for (fruta of canasta) {
+    console.log(`${i}. ${fruta}`);
+    i++; //Después de imprimir cada fruta 'i' se incrementa en 1 en la siguiente iteración 
+}
